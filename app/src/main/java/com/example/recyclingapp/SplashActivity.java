af910083e1abc65ebf976425c9d6c1bbc7057715
Adrian <mamaguda@tip.edu.ph>
2023-04-26 22:27:47 +0800

    Comitt for connection of repo

---
+++ app/src/main/java/com/example/recyclingapp/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.recyclingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
