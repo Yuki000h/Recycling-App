d80ed2779546ca6322c500a8f1d8add13e7d8b66
Adrian <mamaguda@tip.edu.ph>
2023-05-06 15:25:11 +0800

    usbaw comitt last 10 days

--- app/src/main/java/com/example/recyclingapp/FormActivity.java
+++ app/src/main/java/com/example/recyclingapp/FormActivity.java
@@ -10,5 +10,7 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.formview);
+
+
     }
 }
